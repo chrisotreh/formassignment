fde6398ae3d477e5c3bebf3f11dff72c393612ce
christian.yulius <christian.yulius@id.abb.com>
2020-09-23 23:09:22 +0800

    First push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('f', { static: false }) subsForm: NgForm;
   title = 'formassignment';
+  subscription = ["Basic", "Advanced", "Pro"];
+  defaultSubscription = "Advanced";
+
+  userData = {
+    email: '',
+    password: '',
+    subscription: ''
+  }
+  submitted = false;
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  onSubmit() {
+    this.submitted=true;
+    this.userData.email=this.subsForm.value.email;
+    this.userData.password=this.subsForm.value.password;
+    this.userData.subscription=this.subsForm.value.subscription;
+    this.subsForm.reset();
+  }
 }
